0a7abc839375e01e4aa113de2e4ce1f4ce3a2ebf
Stepan <stepanmalyutin2006@gmail.com>
2025-03-04 08:39:46 +0300

    class1

--- 2.cpp
+++ 2.cpp
@@ -1,3 +1,28 @@
-int func(){
-    
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+class bublik
+{
+    public: 
+        int age;
+        char name[20];
+};
+
+int main()
+{
+    char k[] = "chingis";
+    bublik* bubliki = new bublik[5];
+    for(int i = 0; i < 5; i++){
+        (bubliki + i)->age = i;
+        strcpy((bubliki+i)->name, k);
+    }
+    std::cout << "Объект записан" << std::endl;
+    for(int i = 0; i < 5; i++){
+        std::cout << (bubliki + i)->age << std::endl;
+        std::cout << (bubliki+i)->name << std::endl;
+    }
+    delete[] bubliki;
+    std::cout << "Массив объектов удален";
+    return 0;
 }
