e04ebeb8a7abcddc262cd9a814f141ec176191ee
Stepan <stepanmalyutin2006@gmail.com>
2025-03-04 08:47:56 +0300

    Merge branch 'readme' of https://github.com/karapax77/TestGit into readme

--- 2.cpp
+++ 2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <string.h>
 
@@ -8,19 +7,11 @@ class bublik
     public: 
         int age;
         char name[20];
-=======
-
-class bublik
-{
-    int age;
-    char name[20];
->>>>>>> 0c1117198684b72fb9c90a25c71c951b3c3c4e0d
 };
 
 int main()
 {
     char k[] = "chingis";
-<<<<<<< HEAD
     bublik* bubliki = new bublik[5];
     for(int i = 0; i < 5; i++){
         (bubliki + i)->age = i;
@@ -33,8 +24,5 @@ int main()
     }
     delete[] bubliki;
     std::cout << "Массив объектов удален";
-=======
-    
->>>>>>> 0c1117198684b72fb9c90a25c71c951b3c3c4e0d
     return 0;
 }
